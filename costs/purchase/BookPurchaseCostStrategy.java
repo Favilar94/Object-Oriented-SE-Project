de58fa1c1917003afcab0fd04a418a4d786837f3
favilar16 <alejandro.favila@train.ia.center>
2025-12-10 23:18:24 -0600

    Music and Books added, changed the signature of Movie and VideoGame in order to standarize the order of parameters

--- costs/purchase/BookPurchaseCostStrategy.java
+++ costs/purchase/BookPurchaseCostStrategy.java
@@ -1,15 +1,21 @@
 package costs.purchase;
 
+import items.BookType;
 import items.Version;
 
 public class BookPurchaseCostStrategy implements PurchaseCostStrategy{
     
     @Override
     public double getCost(String itemType) {
-        if(itemType.equals(Version.STANDARD.toString())){
+        if(itemType.equals(BookType.PAPERBACK.toString())){
             return 20.0;
 
-        }else{
+        }
+        else if(itemType.equals(BookType.HARDCOVER.toString())){
+            return 35.0;
+
+        }
+        else{
             throw new IllegalArgumentException("Unknown Book version: " + itemType);
         }
     }
